d8ba7f2d7e698b5748aa00406172004a39cf9006
Melisa Tanrıkulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2021-12-03 23:44:18 +0300

    Module 2
    Console.WriteLine(), Console.Write(), Console.ReadLine(), comment command (//) and new line command (\n) are practiced.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("A line");
+            Console.Write(", Not a new line");
+
+            Console.WriteLine("This is a line");
+            Console.WriteLine("This is also a line");
+
+            Console.Write("\nThis is a line");
+            Console.Write("\nThis is the last line");
+            Console.ReadLine();
+        }
+    }
+}
