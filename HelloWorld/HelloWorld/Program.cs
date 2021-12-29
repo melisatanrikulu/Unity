83f4bbc74809b9667d7679873d7f2c003d009f08
Melisa Tanrıkulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2021-12-29 20:12:19 +0300

    Module 5
    Creating a class and a function, creating a new instance of that class and using its functions, and Math class are practiced.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,69 +8,33 @@ namespace HelloWorld
 {
     class Program
     {
-        static int a;
-        static float b;
-        static bool c;
-        static string d;
 
         static void Main(string[] args)
         {
-            var total = true; // needs to be initialized, can be assigned to any type and the type cannot change
+            NewClass newObj = new NewClass(); // can insantiate nonstatic classes, Console is a static class, so it cannot be instantiated
+            newObj.first = 20;
+            newObj.second = 40;
 
-            a = 1;
-            b = 1.1f; // unsigned integers, long, float, double, decimal // u,l,f,d,m, 
-            // The range changes
-            c = true;
-            d = "more than 1 characters";
+            Console.WriteLine(newObj.first + newObj.second);
+            Console.WriteLine(newObj.getSum()); // calls a function of the instance object
 
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(4.0m);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(@"C:\Users\HP\Desktop"); // Escape character is seen as a part of the string (called verbatim)
-            // \n, \t and all such escape characters are read as strings now
+            // Math class
+            Console.WriteLine(Math.Pow(10f, 2f));
+            Console.WriteLine(Math.Min(5, 7));
+            Console.WriteLine(Math.Abs(-25.4));
 
-            // String concatenation
-            string hello = "Hello ";
-            string helloWorld = hello + "World!";
-
-            string ex_one = $"{hello}World!"; // Uses variables in strings
-            string ex_two = $@"C:\Users\HP\Desktop\{helloWorld}.pdf"; // Uses verbatim
-
-            Console.WriteLine(ex_one);
-            Console.WriteLine(ex_two);
-
-            int first = 30;
-            int second = 40;
-
-            Console.WriteLine(first + second + " : adds these two numbers");
-            Console.WriteLine("concatenates these two numbers: " + second + 20); // if the addition is at the beggining,
-            // this command adds those two numbers
-            Console.WriteLine((first + 10) + " : adds these two numbers");
-
-            int sum = first + second;
-            int sub = first - second;
-            int mul = first * second;
-            int div = first / second;
-            float divFloat = 30f / 40;
-            float division = (float) 30 / 40; // casting
-
-            Console.WriteLine("Sum: " + sum + "\nSubstraction: " + sub + "\nMultiplication: " + mul + "\nDivision: " + div + "\nDivision with precision: " + divFloat);
-
-            int third = 25;
-            int fourth = 62;
-
-            third += 10;
-            third -= 5;
-            third *= 4;
-            third /= 3;
-
-            fourth++;
-            ++fourth;
-            fourth--;
-            --fourth;
             Console.ReadLine();
+        }
+
+        class NewClass
+        {
+            public int first; // if this variable is private, it cannot be seen from the outside of the class
+            public int second;
+
+            public int getSum() // a function that returns an integer and has no parameter
+            {
+                return first + second;
+            }
         }
     }
 }
