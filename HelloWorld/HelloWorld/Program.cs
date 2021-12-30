2c29e6665bccd83818b990829f8427f335424dbb
Melisa Tanrıkulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2021-12-31 00:43:08 +0300

    Module 6
    Random class, if, else if, else blocks, arrays, foreach loop, conditional statements, local and global variables, creating classes, switch cases, for, do-while and while loops are practiced.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloWorld.Sum;
 
 namespace HelloWorld
 {
@@ -11,30 +12,159 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            NewClass newObj = new NewClass(); // can insantiate nonstatic classes, Console is a static class, so it cannot be instantiated
-            newObj.first = 20;
-            newObj.second = 40;
 
-            Console.WriteLine(newObj.first + newObj.second);
-            Console.WriteLine(newObj.getSum()); // calls a function of the instance object
+            // Random class
 
-            // Math class
-            Console.WriteLine(Math.Pow(10f, 2f));
-            Console.WriteLine(Math.Min(5, 7));
-            Console.WriteLine(Math.Abs(-25.4));
+            Random die = new Random();
+
+            int first = die.Next(1, 21);
+            int second = die.Next(1, 21);
+            int third = die.Next(1, 21);
+            int fourth = die.Next(1, 21);
+            int fifth = die.Next(1, 21);
+
+            float avg = (first + second + third) / 3f;
+
+            // If, else if, else blocks
+
+            if ( avg > 15 )
+            {
+                Console.WriteLine("Huge damage is done");
+            }
+
+            else if ( 10 < avg && avg <= 15)
+            {
+                Console.WriteLine("Average damage is done");
+            }
+
+            else if ( 5 < avg && avg <= 10 )
+            {
+                Console.WriteLine("No damage is done");
+            }
+
+            else
+            {
+                Console.WriteLine("Done damage to yourself");
+            }
+
+            if ( fourth >= 20 || fifth >= 2 )
+            {
+                Console.WriteLine("Extra damage is done");
+            }
+
+            Console.WriteLine(first + " " + second + " " + third + " " + fourth + " " + fifth);
+            Console.WriteLine(avg);
+
+            // Arrays
+
+            string[] orderNo = new string[3];
+            string[] orders = { "bag", "tshirt", "shoes" };
+            int[] prices = { 350, 200, 600 };
+            int totalPrice = 0;
+            int count = 0;
+
+            orderNo[0] = "GHDF521";
+            orderNo[1] = "TYF95";
+            orderNo[2] = "UKJY984";
+
+            Console.WriteLine(orderNo[2]);
+            Console.WriteLine(orderNo.Length);
+
+            // Foreach loop 
+
+            foreach (int price in prices )
+            {
+                totalPrice += price;
+                count++;
+            }
+
+            Console.WriteLine(totalPrice + " " + count );
+
+            // Conditional statements
+
+            int number = 5;
+            string name = "Melisa Tanrikulu";
+
+            Console.WriteLine("a" == "a");
+            Console.WriteLine(1.2f == 1.2d);
+            Console.WriteLine(1 != number);
+            Console.WriteLine(name.Contains("Mel"));
+
+            Console.WriteLine(number < 6 ? "Less than 6" : "Greater than 6"); // conditional statement
+
+            // Creating new classes
+
+            int num = 0;
+            num = Addition.Add(10);
+            Console.WriteLine(num);
+
+            // Switch Case
+
+            int roommateCount = 0;
+            string status = "";
+            Random rand = new Random();
+            roommateCount = rand.Next(0, 5);
+
+            switch (roommateCount)
+            {
+                case 1:
+                    status = "Lonely";
+                    break;
+                case 2:
+                    status = "Personal space";
+                    break;
+                case 3:
+                    status = "Ideal";
+                    break;
+                case 4:
+                    status = "Might be a problem";
+                    break;
+                default:
+                    status = "You didn't count yourself";
+                    break;
+            }
+
+            Console.WriteLine(status);
+
+            // For loop
+
+            for ( int i = 0; i < 10; i++ )
+            {
+                Console.WriteLine(i);
+            }
+
+            for ( int i = 0; i < orderNo.Length; i++)
+            {
+                Console.WriteLine(orderNo[i]);
+            }
+
+            // Do-While, While loops
+
+            number = 0;
+
+            do
+            {
+                number++;
+
+                if (number == 7)
+                    continue; // Discards the rest of the loop
+
+                Console.WriteLine(number);
+            } while (number < 10);
 
             Console.ReadLine();
         }
+    }
+}
 
-        class NewClass
+namespace HelloWorld.Sum
+{
+    class Addition
+    {
+        public static int Add(int a)
         {
-            public int first; // if this variable is private, it cannot be seen from the outside of the class
-            public int second;
-
-            public int getSum() // a function that returns an integer and has no parameter
-            {
-                return first + second;
-            }
+            int b = 50;
+            return a + b;
         }
     }
 }
